07aa21cee90f29d657de1e74033c295a1186eb13
daniel-konoplanski <85637752+daniel-konoplanski@users.noreply.github.com>
2026-04-25 14:37:30 +0200

    Solution for problem 841

---
+++ src/841_keys_and_rooms/main.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+class Solution
+{
+public:
+    bool canVisitAllRooms(const std::vector<std::vector<int>>& rooms)
+    {
+        int visitedRooms{0};
+
+    }
+
+private:
+    void visit()
+    {
+
+    }
+};
+
+int main()
+{
+    return 0;
+}
